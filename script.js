7f216d3f6a210fd29d07a3d977c795749fe109ae
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-25 10:34:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let targetElement = document.getElementById('level');
+if (targetElement) {
+    let level = 1; 
+	let currentElement = targetElement;
+
+    while (currentElement.parentElement) {
+        level++;
+        currentElement = currentElement.parentElement;
+    }
+	  alert(`The level of the element is: ${level}`);
+	} else {
+    alert("The element with ID 'level' was not found in the DOM.");
+}
